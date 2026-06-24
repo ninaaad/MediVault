07e16ab789fa23fe7825e819b5b517f8a8bcd779
ninaaad <ninad.gawade2005@gmail.com>
2026-06-24 16:14:25 +0530

    Set up Express and PostgreSQL Connection

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const pool = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+app.get('/api/db-test', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+
+        res.json({
+            success: true,
+            data: result.rows[0]
+        });
+    }
+    catch(error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
+});
+
+app.get('/api/health', (req, res) => {
+    res.json({
+        status: "OK"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+});
