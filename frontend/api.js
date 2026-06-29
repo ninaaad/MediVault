072d429b3536e96ef4b94f68bed18573b8ac07b0
ninaaad <ninad.gawade2005@gmail.com>
2026-06-29 11:07:54 +0530

    add start script to package.json

--- frontend/api.js
+++ frontend/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://medivault-backend-fmcd.onrender.com/api';
 
 function getToken() {
     return localStorage.getItem('mediVaultToken');
